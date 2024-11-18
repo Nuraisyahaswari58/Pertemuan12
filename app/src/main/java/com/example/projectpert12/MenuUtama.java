bcf05284e4fbdcae44fc566d26fadaa79ffbe134
Nuraisyahaswari58 <nuraisyahaswari@students.polmed.ac.id>
2024-11-18 20:42:55 +0700

    update tampilan about, segitiga, dan aktivasi function class menu utama

--- app/src/main/java/com/example/projectpert12/MenuUtama.java
+++ app/src/main/java/com/example/projectpert12/MenuUtama.java
@@ -8,7 +8,16 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Button;
+import android.widget.Toast;
+import android.content.Intent;
+
 public class MenuUtama extends AppCompatActivity {
+    Button segitiga;
+    Button tentang;
+    Button keluar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +28,31 @@ public class MenuUtama extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        segitiga = findViewById(R.id.btnsegitiga);
+        tentang = findViewById(R.id.btntentang);
+        keluar = findViewById(R.id.btnkeluar);
+
+        segitiga.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MenuUtama.this, Segitiga.class));
+            }
+        });
+
+        tentang.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MenuUtama.this, About.class));
+            }
+        });
+
+        keluar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MenuUtama.this, MainActivity.class));
+            }
         });
     }
 }
